ae9c2699dc66af0fb2f9b5a44712bf504eb19f2e
hugosenari <hugosenari@gmail.com>
2022-03-12 02:14:49 -0300

    chore: re index odsreader

---
+++ v0_1_0/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''OpenDocument Spreadhseet reader'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-odsreader-v0_1_0.flake = false;
+  inputs.src-odsreader-v0_1_0.owner = "dariolah";
+  inputs.src-odsreader-v0_1_0.ref   = "refs/tags/v0.1.0";
+  inputs.src-odsreader-v0_1_0.repo  = "odsreader";
+  inputs.src-odsreader-v0_1_0.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-odsreader-v0_1_0"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-odsreader-v0_1_0";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
